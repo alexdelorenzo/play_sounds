1947cf455273f683154f6f6e6dc0e3d83006c177
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2020-11-15 18:31:02 -0500

    Add paths to exports

--- play_sounds/__init__.py
+++ play_sounds/__init__.py
@@ -1,1 +1,5 @@
+# export asset paths
+from .base import DEFAULT_ASSETS, DEFAULT_SONG, DEFAULT_SOUND
+
+# export functions and context managers
 from .base import play_after, play_loop, play_while_running, play_after
