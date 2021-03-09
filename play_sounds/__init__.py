fb172bf9b6b52d9b58e76892fa2586d65bd8c88a
Alex DeLorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2021-03-09 01:30:24 -0500

    Import async functions

--- play_sounds/__init__.py
+++ play_sounds/__init__.py
@@ -2,8 +2,9 @@
 from .base import DEFAULT_ASSETS, DEFAULT_SONG, DEFAULT_SOUND
 
 # export functions and context managers
-from .base import play_after, play_loop, play_while_running, play_after, \
-  get_assets_dir
+from .base import play_file, play_loop, play_while_running, play_after, \
+  get_assets_dir, play_file_async, play_while_running_async, \
+  play_after_async
 
 # export bell
 from .bell import bell, bell_after
