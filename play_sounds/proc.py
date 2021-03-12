f79d16ac5452230da0bcd6955bd8f403957ffbf6
alexdelorenzo <you@example.com>
2021-03-12 17:15:22 -0500

    Allow users to catch KeyboardInterrupt without triggering exit

--- play_sounds/proc.py
+++ play_sounds/proc.py
@@ -1,11 +1,11 @@
-from typing import Callable, Optional, Set
+from typing import Callable, Optional, Set, Any
 from multiprocessing import Process
 from platform import platform
 from pathlib import Path
 from weakref import finalize as finalizer
 from sys import exit
 import signal
-
+import atexit
 
 PLATFORM: str = platform().lower()
 _PROCS: Set[Process] = set()
@@ -44,11 +44,15 @@ def kill_process(
   proc.kill()
   proc.join()
 
-  if running_procs is not None and proc in running_procs:
+  if running_procs and proc in running_procs:
     running_procs.remove(proc)
 
 
-def kill_child_procs(signum: int, frame):
+def kill_child_procs(
+  signum: Optional[int] = None,
+  frame: Optional[Any] = None,
+  perform_exit: bool = True
+):
   if _PROCS:
     for proc in _PROCS.copy():
       try:
@@ -57,12 +61,18 @@ def kill_child_procs(signum: int, frame):
       except:
         pass
 
-  exit()
+  if perform_exit:
+    exit()
 
 
 def register_handlers():
+  ## allow users to catch KeyboardInterrupt without exiting
+  ## by not registering a signal handler for SIGINT,
+  ## and instead allowing atexit to handle it
+  # signal.signal(signal.SIGINT, kill_child_procs)
+  atexit.register(kill_child_procs, perform_exit=False)
+
   signal.signal(signal.SIGTERM, kill_child_procs)
-  signal.signal(signal.SIGINT, kill_child_procs)
   signal.signal(signal.SIGSEGV, kill_child_procs)
   signal.signal(signal.SIGABRT, kill_child_procs)
   signal.signal(signal.SIGILL, kill_child_procs)
